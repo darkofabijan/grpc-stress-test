24ca7f097dfcc95a21855fbd65ecc63a73b2bad7
Darko Fabijan <darko@renderedtext.com>
2017-07-23 00:22:56 +0200

    Establish connection every time :boom:

--- grpc-elixir/examples/helloworld/priv/client.exs
+++ grpc-elixir/examples/helloworld/priv/client.exs
@@ -1,5 +1,7 @@
-{:ok, channel} = GRPC.Stub.connect("localhost:50051")
 Enum.each 1..100_000, fn(x) ->
+  IO.puts "connecting"
+  {:ok, channel} = GRPC.Stub.connect("localhost:50051")
+  IO.puts "connected"
   IO.puts x
   reply = channel |> Helloworld.Greeter.Stub.say_hello(Helloworld.HelloRequest.new(name: Integer.to_string(x)))
   IO.inspect reply
