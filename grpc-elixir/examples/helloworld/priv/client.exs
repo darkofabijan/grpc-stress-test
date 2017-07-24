c41fd1de7312971ebeb65a3b8db1dd22a322c821
Darko Fabijan <darko.fabijan@gmail.com>
2017-07-24 10:48:34 +0200

    Update client.exs

--- grpc-elixir/examples/helloworld/priv/client.exs
+++ grpc-elixir/examples/helloworld/priv/client.exs
@@ -1,6 +1,6 @@
 {:ok, channel} = GRPC.Stub.connect("localhost:50051")
 Enum.each 1..100_000, fn(x) ->
-  IO.puts x
+  IO.puts "#{DateTime.utc_now} - #{x}"
   reply = channel |> Helloworld.Greeter.Stub.say_hello(Helloworld.HelloRequest.new(name: Integer.to_string(x)))
   IO.inspect reply
 end
