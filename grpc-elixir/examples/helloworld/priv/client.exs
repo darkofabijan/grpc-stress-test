ec42416513beee1024e020851b1674550193c4c7
Igor Šarčević <igor@renderedtext.com>
2017-08-06 17:19:39 +0200

    Cowboy adapter

--- grpc-elixir/examples/helloworld/priv/client.exs
+++ grpc-elixir/examples/helloworld/priv/client.exs
@@ -1,4 +1,4 @@
-{:ok, channel} = GRPC.Stub.connect("localhost:50051")
+{:ok, channel} = GRPC.Stub.connect("localhost:50051", adapter: GRPC.Adapter.Cowboy)
 Enum.each 1..100_000, fn(x) ->
   IO.puts x
   reply = channel |> Helloworld.Greeter.Stub.say_hello(Helloworld.HelloRequest.new(name: Integer.to_string(x)))
